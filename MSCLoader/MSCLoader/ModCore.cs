964a6ae6e4c1004e3ae99ada69ec93bd1c06c62c
Piotr Wroczyński <piotr.wwww@gmail.com>
2017-08-25 23:55:10 +0200

    core assets + small changes

---
+++ MSCLoader/MSCLoader/ModCore.cs
@@ -0,0 +1,11 @@
+﻿//Core class (placeholder for core assets)
+namespace MSCLoader
+{
+    public class ModCore : Mod
+    {
+        public override string ID => "MSCLoader_Core"; 
+        public override string Name => "MSCLoader"; 
+        public override string Author => "Piotrulos";
+        public override string Version => ModLoader.Version;
+    }
+}
