05d1bb24a5884b4d0b6168c7e5d963f57a4bed38
Piotr Wroczyński <piotr.wwww@gmail.com>
2017-10-06 20:19:32 +0200

    small changes

--- MSCLoader/MSCLoader/ModCore.cs
+++ MSCLoader/MSCLoader/ModCore.cs
@@ -8,6 +8,7 @@ namespace MSCLoader
         public override string Name => "MSCLoader"; 
         public override string Author => "Piotrulos";
         public override string Version => ModLoader.Version;
+        public override bool UseAssetsFolder => true;
     }
 #pragma warning restore CS1591
 }
