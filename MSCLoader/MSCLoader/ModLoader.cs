cdf827c1ae03010a1c982e61000609c1bcb3e11e
piotrulos <piotr.wwww@gmail.com>
2017-11-17 01:39:02 +0100

    mp3 library test

--- MSCLoader/MSCLoader/ModLoader.cs
+++ MSCLoader/MSCLoader/ModLoader.cs
@@ -69,7 +69,7 @@ namespace MSCLoader
         static string ConfigFolder = Path.Combine(ModsFolder, @"Config\");
         static string AssetsFolder = Path.Combine(ModsFolder, @"Assets\");
 
-        static bool experimental = false; //Is this build is experimental
+        static bool experimental = true; //Is this build is experimental
         static bool modStats = false;
         static GameObject mainMenuInfo;
         static Animator menuInfoAnim;
