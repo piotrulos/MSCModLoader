33c3334921d055b64e61104ee15239e9ec64da90
piotrulos <piotr.wwww@gmail.com>
2018-02-03 01:41:44 +0100

    64-bit patch, debug fixes, tested.

--- MSCPatcher/MSCPatcher/MD5FileHashes.cs
+++ MSCPatcher/MSCPatcher/MD5FileHashes.cs
@@ -17,5 +17,24 @@ namespace MSCPatcher
         public const string mono32debug = "62629B5CA9C1BBE3C97A3E8C91D05D3A";
         public const string mono64normal = "F190C7ECFE414FB407137C1D95AC310E";
         public const string mono64debug = "3EE5C3BD42B61AE820A028AF35DF99C0";
+
+        /*
+         * mysummercar.exe
+         * 
+         * E9F1D7E11359DA995881B9280E1726B3 (32-bit)
+         * 0B782168216B6B614D6C63485DAE20B0 (64-bit)
+         * 
+         * CSteamworks.dll
+         * 
+         * 4802608A59A9D268EF94A5C0727EC777 (32-bit)
+         * B7F58E5AD108BFEDC1F90CD3525AD29A (64-bit)
+         */
+
+        public const string exe32 = "E9F1D7E11359DA995881B9280E1726B3";
+        public const string exe64 = "0B782168216B6B614D6C63485DAE20B0";
+        public const string steam32 = "4802608A59A9D268EF94A5C0727EC777";
+        public const string steam64 = "B7F58E5AD108BFEDC1F90CD3525AD29A";
+
+
     }
 }
