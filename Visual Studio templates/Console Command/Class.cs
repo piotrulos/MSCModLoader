8b366abd0361204c46f13487069b29b2e85fe7f9
piotrulos <piotr.wwww@gmail.com>
2018-05-13 00:06:59 +0200

    VS templates update

---
+++ Visual Studio templates/Console Command/Class.cs
@@ -0,0 +1,20 @@
+﻿using MSCLoader;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ConsoleCommand
+    {
+      	// What the player has to type into the console to execute your commnad
+    	public override string Name => "$safeitemname$";
+
+        // The help that's displayed for your command when typing help
+        public override string Help => "Command Description";
+
+        // The function that's called when executing command
+        public override void Run(string[] args)
+        {
+            //Do something when command is executed
+        }
+
+    }
+}
