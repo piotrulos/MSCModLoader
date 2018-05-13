6e5d8083099b9bb8f1e011debb8c50e3f8bb1b39
Piotr Wroczyński <piotr.wwww@gmail.com>
2018-05-13 14:25:17 +0200

    Recompiled patcher binary (#50)

--- MSCPatcher/MSCPatcher/Properties/AssemblyInfo.cs
+++ MSCPatcher/MSCPatcher/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kosmo Software")]
 [assembly: AssemblyProduct("MSCPatcher")]
-[assembly: AssemblyCopyright("Copyright © 2017 by Kosmo Software")]
+[assembly: AssemblyCopyright("Copyright © 2018 by Kosmo Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2.136")]
-[assembly: AssemblyFileVersion("0.4.2.136")]
+[assembly: AssemblyVersion("0.4.2.138")]
+[assembly: AssemblyFileVersion("0.4.2.138")]
 [assembly: NeutralResourcesLanguage("en")]
 
