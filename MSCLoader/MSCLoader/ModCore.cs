d0bf81fbc2f6efdadcbc4094c657f0ba570f05d2
piotrulos <piotr.wwww@gmail.com>
2019-01-17 01:26:57 +0100

    Settings Redesign - part 2

--- MSCLoader/MSCLoader/ModCore.cs
+++ MSCLoader/MSCLoader/ModCore.cs
@@ -5,7 +5,7 @@ namespace MSCLoader
     public class ModCore : Mod
     {
         public override string ID => "MSCLoader_Core"; 
-        public override string Name => "MSCLoader"; 
+        public override string Name => "MSCLoader (Assets)"; 
         public override string Author => "Piotrulos";
         public override string Version => ModLoader.Version;
         public override bool UseAssetsFolder => true;
