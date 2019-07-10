e131bbf40087d24763b38ff3a8a2e74aa1a72c94
piotrulos <piotr.wwww@gmail.com>
2019-07-10 17:49:44 +0200

    Warn about wrong runtime, source of some mod crashes (if wrong target)

--- MSCLoader/MSCLoader/Properties/AssemblyInfo.cs
+++ MSCLoader/MSCLoader/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.178")]
-[assembly: AssemblyFileVersion("1.0.1.178")]
+[assembly: AssemblyVersion("1.0.1.179")]
+[assembly: AssemblyFileVersion("1.0.1.179")]
