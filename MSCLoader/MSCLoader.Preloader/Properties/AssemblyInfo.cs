9ea0647e97c11b5cc0051456ad8198ff5d5a684b
piotrulos <piotr.wwww@gmail.com>
2021-07-03 23:28:34 +0200

    Decoupled Main (base for self-update)

--- MSCLoader/MSCLoader.Preloader/Properties/AssemblyInfo.cs
+++ MSCLoader/MSCLoader.Preloader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSCLoader")]
-[assembly: AssemblyDescription("My Summer Car Mod Loader")]
+[assembly: AssemblyTitle("MSCLoader.Preloader")]
+[assembly: AssemblyDescription("Preloader and core components for MSCLoader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kosmo Software")]
-[assembly: AssemblyProduct("MSCLoader")]
+[assembly: AssemblyProduct("MSCLoader.Preloader")]
 [assembly: AssemblyCopyright("GPL 3.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3323fe4a-d360-4692-9309-f20b57bbb745")]
+[assembly: Guid("807b36c0-f066-4736-8980-e7b18e3cb1f9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.16.222")]
-[assembly: AssemblyFileVersion("1.1.16.222")]
+[assembly: AssemblyVersion("1.0.0.222")]
+[assembly: AssemblyFileVersion("1.0.0.222")]
