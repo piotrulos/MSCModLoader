3d6357c97138348b0343023956764f378338ecf1
piotrulos <piotr.wwww@gmail.com>
2021-07-17 20:13:24 +0200

    Preloader custom log

---
+++ MSCLoader/MSCLoader.Preloader/MDebug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MSCLoader.Preloader
+{
+    class MDebug
+    {
+        private static TraceSource ts = new TraceSource("MSCLoader");
+        private static TextWriterTraceListener tw = new TextWriterTraceListener("MSCLoader_Preloader.txt");
+
+        public static void Init()
+        {
+            ts.Switch.Level = SourceLevels.All;       
+            ts.Listeners.Add(tw);
+            Log("MSCLoader Preloader Log");
+            Log(DateTime.Now.ToString("u"), true);
+        }
+
+        public static void Log(string message, bool newline = false)
+        {
+            tw.WriteLine(message);
+            if (newline) tw.WriteLine("");
+            tw.Flush();
+        }
+    }
+}
