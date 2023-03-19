0029f1660aba7893619c38c7f005cf2061d0d2f7
ThatRedKite <48386182+ThatRedKite@users.noreply.github.com>
2023-03-19 20:30:20 +0100

    Fix some typos and grammar (#165)
    * Fixed some typos in comments
    
    I fixed some typos / wonky sounding sentences
    
    * Slight rewording
    
    * Update README.md
    
    * Reword some strings
    
    * I tried to make them sound more natural and easy to understand. I also changed some capitalisation to make it correct (e. g. steam -> Steam)
    
    * Slight rewording
    
    * Update ConsoleController.cs
    
    * Fixed Typos/Grammar
    
    * I tried to fix grammar and some typos to make some messages more clear to the users. I also tried to fix the capitalisation but i might have "fixed" them in a string that gets put into `.toUpper()` which does not change anything, lol.

--- Visual Studio templates/MSCLoader Mod/Class1.cs
+++ Visual Studio templates/MSCLoader Mod/Class1.cs
@@ -5,11 +5,11 @@ namespace $safeprojectname$
 {
     public class $safeprojectname$ : Mod
     {
-        public override string ID => "$safeprojectname$"; //Your mod ID (unique)
-        public override string Name => "$modName$"; //You mod name
-        public override string Author => "$modAuthor$"; //Your Username
-        public override string Version => "$modVersion$"; //Version
-        public override string Description => ""; //Short description of your mod
+        public override string ID => "$safeprojectname$"; // Your (unique) mod ID 
+        public override string Name => "$modName$"; // Your mod name
+        public override string Author => "$modAuthor$"; // Name of the Author (your name)
+        public override string Version => "$modVersion$"; // Version
+        public override string Description => ""; // Short description of your mod
 
         public override void ModSetup()
         {$if$ ($setOnMenuLoad$ == true)
@@ -28,20 +28,20 @@ namespace $safeprojectname$
         private void Mod_Settings()
         {
             // All settings should be created here. 
-            // DO NOT put anything else here that settings or keybinds
+            // DO NOT put anything that isn't settings or keybinds in here!
         }
         $if$ ($setOnMenuLoad$ == true)
         private void Mod_OnMenuLoad()
         {
-            // Called once, when mod is loaded in main menu
+            // Called once, when the mod is loaded in the main menu
         }$endif$ $if$ ($setOnNewGame$ == true)
         private void Mod_OnNewGame()
         {
-            // Called once, when creating new game, you can delete your mod save here
+            // Called once, when creating a new game. This is useful for deleting old mod saves
         }$endif$ $if$ ($setPreLoad$ == true)
         private void Mod_PreLoad()
         {
-            // Called once, right after GAME scene loads but before game is fully loaded
+            // Called once, right after GAME scene loads but before the game is fully loaded
         }$endif$ $if$ ($setOnLoad$ == true)
         private void Mod_OnLoad()
         {
