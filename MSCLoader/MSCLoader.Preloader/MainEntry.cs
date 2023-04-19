3ef293f0c219693d01aa0b2201c2d9ff7d7eebdb
piotrulos <piotr.wwww@gmail.com>
2023-04-19 12:51:43 +0200

    Doorstop 4 (linux fixes)

--- MSCLoader/MSCLoader.Preloader/MainEntry.cs
+++ MSCLoader/MSCLoader.Preloader/MainEntry.cs
@@ -7,6 +7,19 @@ using IniParser;
 using Ionic.Zip;
 using System.Linq;
 
+namespace Doorstop
+{
+    //Entry point for doorstop 4.0
+    class Entrypoint
+    {
+        public static void Start()
+        {
+            //Just point to doorstop 3.x entry point
+            MSCLoader.Preloader.MainEntry.Main();
+        }
+    }
+}
+
 namespace MSCLoader.Preloader
 {
     public static class MainEntry
@@ -15,7 +28,7 @@ namespace MSCLoader.Preloader
         static long offset = 0;
 
         //Entry point for doorstop
-        public static void Main(string[] args)
+        public static void Main()
         {
             string[] launchArgs = System.Environment.GetCommandLineArgs(); //Environment CommandLine Arguments (in Main there are doorstop args only)
 
@@ -36,17 +49,17 @@ namespace MSCLoader.Preloader
         }
         private static void UnpackUpdate()
         {
-            if (File.Exists(Path.Combine("Updates", @"Core\update.zip")))
+            if (File.Exists(Path.Combine("Updates", Path.Combine("Core", "update.zip"))))
             {
-                string modPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"MySummerCar\Mods");
-                string managedPath = Path.Combine("mysummercar_Data", @"Managed");
+                string modPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), Path.Combine("MySummerCar", "Mods"));
+                string managedPath = Path.Combine("mysummercar_Data", "Managed");
                 switch (cfg)
                 {
                     case "GF":
-                        modPath = Path.GetFullPath(Path.Combine("Mods", "")); 
+                        modPath = Path.GetFullPath(Path.Combine("Mods", ""));
                         break;
                     case "MD":
-                        modPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"MySummerCar\Mods");
+                        modPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), Path.Combine("MySummerCar", "Mods"));
                         break;
                     case "AD":
                         modPath = Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"..\LocalLow\Amistech\My Summer Car\Mods"));
@@ -56,9 +69,9 @@ namespace MSCLoader.Preloader
                         break;
                 }
                 MDebug.Log("Installing modloader update...");
-                UnpackZip(Path.Combine("Updates", @"Core\update.zip"), Path.Combine("Updates", "Core"));
-                UnpackZip(Path.Combine("Updates", @"Core\Managed.zip"), managedPath);
-                UnpackZip(Path.Combine("Updates", @"Core\Mods.zip"), modPath);
+                UnpackZip(Path.Combine("Updates", Path.Combine("Core", "update.zip")), Path.Combine("Updates", "Core"));
+                UnpackZip(Path.Combine("Updates", Path.Combine("Core", "Managed.zip")), managedPath);
+                UnpackZip(Path.Combine("Updates", Path.Combine("Core", "Mods.zip")), modPath);
                 MDebug.Log("Installing modloader update done!");
             }
         }
@@ -127,9 +140,9 @@ namespace MSCLoader.Preloader
             {
                 bool enLog = false;
                 bool skipCfg = false;
-                offset = FindBytes(Path.Combine("", @"mysummercar_Data\mainData"), data);
+                offset = FindBytes(Path.Combine("", Path.Combine("mysummercar_Data","mainData")), data);
 
-                using (FileStream stream = File.OpenRead(Path.Combine("", @"mysummercar_Data\mainData")))
+                using (FileStream stream = File.OpenRead(Path.Combine("", Path.Combine("mysummercar_Data","mainData"))))
                 {
                     MDebug.Log("Checking output_log status...");
                     stream.Position = offset + 115;
@@ -156,7 +169,7 @@ namespace MSCLoader.Preloader
                 }
                 if (enLog)
                 {
-                    using (FileStream stream = new FileStream(Path.Combine("", @"mysummercar_Data\mainData"), FileMode.Open, FileAccess.ReadWrite))
+                    using (FileStream stream = new FileStream(Path.Combine("", Path.Combine("mysummercar_Data","mainData")), FileMode.Open, FileAccess.ReadWrite))
                     {
                         //output_log.txt
                         MDebug.Log("Enabling output_log...");
@@ -167,7 +180,7 @@ namespace MSCLoader.Preloader
                 }
                 if (cfgScreenSkip != skipCfg)
                 {
-                    using (FileStream stream = new FileStream(Path.Combine("", @"mysummercar_Data\mainData"), FileMode.Open, FileAccess.ReadWrite))
+                    using (FileStream stream = new FileStream(Path.Combine("", Path.Combine(@"mysummercar_Data","mainData")), FileMode.Open, FileAccess.ReadWrite))
                     {
                         MDebug.Log("Changing config screen skip...");
                         stream.Position = offset + 96;
