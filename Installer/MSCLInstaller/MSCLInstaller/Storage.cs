edb9cdd932a635ec502dc0f8dcd5c8623e05af92
83117110115101116 <202849979+83117110115101116@users.noreply.github.com>
2026-01-13 19:04:08 +0200

    Removed unnecessary variable packFiles bcz it's not used

--- Installer/MSCLInstaller/MSCLInstaller/Storage.cs
+++ Installer/MSCLInstaller/MSCLInstaller/Storage.cs
@@ -24,7 +24,6 @@
         public static Game selectedGame = Game.MSC;
         public static bool is64 = true; //MSC only
         public static bool dbgPack = false;
-        public static string[] packFiles = null;
         public static SelectedAction selectedAction = SelectedAction.Nothing;
         public static bool skipIntroCfg = false;
         public static bool skipConfigScreenCfg = false;
